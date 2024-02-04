4ad7321f35b05a262a4dab06b86cd7ce73d68b56
Tim Lines <timlines89@gmail.com>
2024-02-04 11:23:14 -0600

    transposeTwoStrings best test case

--- transposeTwoStrings/js/main.js
+++ transposeTwoStrings/js/main.js
@@ -1,29 +1,40 @@
-function transposeTwoStrings (array){
+// function transposeTwoStrings (array){
 
 
+//     let result = [];
+
+//     // compare the two string lengths
+//     let longerString = '';
+//     if ( array[0].length < array[1].length ){
+//         longerString = array[1];
+//     }else{
+//         longerString = array[0];
+//     }
+
+//     // add the characters to an array
+//     for ( let i = 0; i < longerString.length; i++ ){
+
+
+//         result.push( (array[0][i] == undefined ? ` ` : `${array[0][i]}`) + (array[1][i] == undefined ? `  ` : ` ${array[1][i]}`));
+
+//     } 
+
+//     return result.join('\n');
+
+
+// }
+
+
+function transposeTwoStrings(arr){
     let result = [];
+    let len = Math.max(arr[0].length, arr[1].length);
 
-    // compare the two string lengths
-    let longerString = '';
-    if ( array[0].length < array[1].length ){
-        longerString = array[1];
-    }else{
-        longerString = array[0];
+    for (let i = 0; i < len; i++){
+        let column = (arr[0][i] || ' ') + ' ' + (arr[1][i] || ' ');
+        result.push(column);
     }
 
-    // add the characters to an array
-    for ( let i = 0; i < longerString.length; i++ ){
-        // console.log(`${array[0][i]} ${array[1][i]}`)
-        if ( array[0][i] == undefined || array[1][i] == undefined){
-            ' '
-        }
-
-        result.push( (array[0][i] == undefined ? ` ` : `${array[0][i]}`) + (array[1][i] == undefined ? `  ` : ` ${array[1][i]}`));
-
-    } 
-
     return result.join('\n');
-
 
 }
 
