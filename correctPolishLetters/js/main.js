d878b2be9b2ec3b1d31dd557e7e85178672a4cc1
Tim Lines <timlines89@gmail.com>
2024-02-19 09:47:15 -0600

    correctPolish Letters

---
+++ correctPolishLetters/js/main.js
@@ -0,0 +1,27 @@
+ą -> a,
+ć -> c,
+ę -> e,
+ł -> l,
+ń -> n,
+ó -> o,
+ś -> s,
+ź -> z,
+ż -> z
+
+
+function correctPolishLettters(string){
+    let newString = string.replaceAll('ą', 'a')
+    newString = newString.replaceAll('ć', 'c')
+    newString = newString.replaceAll('ę', 'e')
+    newString = newString.replaceAll('ł', 'l')
+    newString = newString.replaceAll('ń', 'n')
+    newString = newString.replaceAll('ó', 'o')
+    newString = newString.replaceAll('ś', 's')
+    newString = newString.replaceAll('ź', 'z')
+    newString = newString.replaceAll('ż', 'z')
+
+    return newString;
+}
+
+
+console.log()
