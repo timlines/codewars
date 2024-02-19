be979f6592fbbfc0110f4d0fb7cdba5288be83d4
Tim Lines <timlines89@gmail.com>
2024-02-18 22:27:13 -0600

    sevenAte9 w/ regex

--- sevenAte9/js/main.js
+++ sevenAte9/js/main.js
@@ -1,6 +1,12 @@
+// function sevenAte9(str){
+//     return str.replaceAll('797', '77').replaceAll('797', '77')
+// }
+
 function sevenAte9(str){
-    return str.replaceAll('797', '77').replaceAll('797', '77')
+    return str.replace(/79(?=7)/g, '7')
 }
 
 // console.log(sevenAte9('79712312'))
 console.log(sevenAte9('779797')) // 7777
+
+
