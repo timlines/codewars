a4a71698ec2187e814b2820c4a9e61ee71cd35e4
Tim Lines <timlines89@gmail.com>
2024-02-20 09:52:42 -0600

    correctPolish Letters using split map and join

--- correctPolishLetters/js/main.js
+++ correctPolishLetters/js/main.js
@@ -1,27 +1,47 @@
-ą -> a,
-ć -> c,
-ę -> e,
-ł -> l,
-ń -> n,
-ó -> o,
-ś -> s,
-ź -> z,
-ż -> z
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
 
 
-function correctPolishLettters(string){
-    let newString = string.replaceAll('ą', 'a')
-    newString = newString.replaceAll('ć', 'c')
-    newString = newString.replaceAll('ę', 'e')
-    newString = newString.replaceAll('ł', 'l')
-    newString = newString.replaceAll('ń', 'n')
-    newString = newString.replaceAll('ó', 'o')
-    newString = newString.replaceAll('ś', 's')
-    newString = newString.replaceAll('ź', 'z')
-    newString = newString.replaceAll('ż', 'z')
+// function correctPolishLettters(string){
+//     let newString = string.replaceAll('ą', 'a')
+//     newString = newString.replaceAll('ć', 'c')
+//     newString = newString.replaceAll('ę', 'e')
+//     newString = newString.replaceAll('ł', 'l')
+//     newString = newString.replaceAll('ń', 'n')
+//     newString = newString.replaceAll('ó', 'o')
+//     newString = newString.replaceAll('ś', 's')
+//     newString = newString.replaceAll('ź', 'z')
+//     newString = newString.replaceAll('ż', 'z')
 
-    return newString;
+//     return newString;
+// }
+
+
+// v2
+
+
+const polishLetters = {
+    'ą': 'a',
+    'ć': 'c',
+    'ę': 'e',
+    'ł': 'l',
+    'ń': 'n',
+    'ó': 'o',
+    'ś': 's',
+    'ź': 'z',
+    'ż': 'z',
+};
+
+function correctPolishLetters(string){
+    return string.split('').map((char) => polishLetters[char] || char).join('');
 }
 
 
-console.log()
+console.log(correctPolishLetters('Jędrzej Błądziński'))
