184ee0d5afe52e837d2f104e4f1fa2c35ed3b9f0
Tim Lines <timlines89@gmail.com>
2024-07-23 23:53:01 -0500

    lowercaseCount

---
+++ lowercaseCount/js/main.js
@@ -0,0 +1,28 @@
+// Your task is simply to count the total number of lowercase letters in a string.
+
+// Examples
+// "abc" ===> 3
+
+// "abcABC123" ===> 3
+
+// "abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 3
+
+// "" ===> 0;
+
+// "ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 0
+
+// "abcdefghijklmnopqrstuvwxyz" ===> 26
+
+function lowercaseCount(str){
+    let count = 0;
+    for (let char of str){
+        if (char >= 'a' && char <= 'z') {
+            count++;
+        }
+    }
+    return count;
+
+}
+
+const string = 'Hello World';
+console.log(lowercaseCount(string)); // 8
