cdf0a58c721386aed6a7a0e91fba69b86634d67f
Tim Lines <timlines89@gmail.com>
2024-07-28 15:39:55 -0500

    smallsID Best Practice

--- smallestunusedID/js/main.js
+++ smallestunusedID/js/main.js
@@ -8,24 +8,24 @@
 
 // Go on and code some pure awesomeness!
 
-function nextId(ids){
-    let sorted = ids.sort((a, b) => a - b);
-    // console.log(sorted);
+// function nextId(ids){
+//     let sorted = ids.sort((a, b) => a - b);
+//     // console.log(sorted);
 
-    for (let i = 0; i < ids.length; i++){
-        if ( ids[0] !== 0 ){
-            return 0;
-        } else if ( ids[i] === ids[i + 1]){
-           //skip
-            
-        }
-        else if (  ids[i] + 1 !== ids[i + 1]){ // 1 !== 1
-            return ids[i] + 1;
-        }
+//     for (let i = 0; i < ids.length; i++){
+//         if ( ids[0] !== 0 ){
+//             return 0;
+//         } else if ( ids[i] === ids[i + 1]){
+//            //skip
+
+//         }
+//         else if (  ids[i] + 1 !== ids[i + 1]){ // 1 !== 1
+//             return ids[i] + 1;
+//         }
      
-    }
+//     }
 
-}
+// }
 
 // console.log(nextId([0,1,2,3,5]));
 // console.log(nextId([0,1,2,3,4,5,6,7,8,9,10]));
@@ -47,3 +47,14 @@ console.log(nextId([1,2,0,2,3,5]));
         //     // console.log( `${ids[i] + 1} This ID is empty`);
         //     return ids[i] + 1;
         // }
+
+function nextId(ids){
+    let x = 0;
+    while (ids.includes(x)) x++;
+    return x;
+}
+
+console.log(nextId([0,1,2,3,5]));
+console.log(nextId([0,1,2,3,4,5,6,7,8,9,10]));
+console.log(nextId([1,2,0,2,3,5]));
+console.log(nextId([4,4,9,6,2,9,9,4]));
