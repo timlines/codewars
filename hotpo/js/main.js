b003e6167d44bd8577fb372ba4da505e50cbf7ac
Tim Lines <timlines89@gmail.com>
2024-12-20 11:33:16 -0600

    hotpo best answer

--- hotpo/js/main.js
+++ hotpo/js/main.js
@@ -1,25 +1,36 @@
-var hotpo = function(n){
+// var hotpo = function(n){
 
-    let result = 0;
+//     let result = 0;
 
-    while ( n > 1){
-        if ( n % 2){
-            n = 3*n + 1;
-        } else {
-            n = n/2;
-        }
+//     while ( n > 1){
+//         if ( n % 2){
+//             n = 3*n + 1;
+//         } else {
+//             n = n/2;
+//         }
 
-        result++;
-        // console.log(result);
+//         result++;
+//         // console.log(result);
+//     }
+
+//     return result;
+    
+//     // if odd ie 1, 3, 5, 
+    
+// }
+
+
+let hotpo = function (n, acc = 0) {
+    if ( <= 1){
+        return acc;
+    }else{
+        return hotpo(n%2==0 ? n/2 : 3*n+1, acc+1);
     }
-
-    return result;
-    
-    // if odd ie 1, 3, 5, 
-    
 }
 
 console.log(hotpo(1));
 console.log(hotpo(5));
 console.log(hotpo(6));
 console.log(hotpo(23));
+
+
