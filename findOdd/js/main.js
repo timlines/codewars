0f8784ba885f5514e698bff8565b56882d1ef4a8
Tim Lines <timlines89@gmail.com>
2025-01-06 10:29:39 -0600

    findOdd 2

--- findOdd/js/main.js
+++ findOdd/js/main.js
@@ -9,29 +9,34 @@
 // [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
-function findOdd(A) {
+// function findOdd(A) {
 
- // sort array
- const array = A.sort((a, b) => a - b);
+//  // sort array
+//  const array = A.sort((a, b) => a - b);
  
 
- // count elements in array
- const counts = {};
- for ( const element of array ) {
-    if ( counts[element] ) {
-        counts[element]++;
-    }else {
-        counts[element] = 1;
-    }
+//  // count elements in array
+//  const counts = {};
+//  for ( const element of array ) {
+//     if ( counts[element] ) {
+//         counts[element]++;
+//     }else {
+//         counts[element] = 1;
+//     }
 
- }
+//  }
 
- // return the odd element
+//  // return the odd element
 
- let odd = Object.keys(counts).find((key) => counts[key] % 2)
- return Number(odd);
+//  let odd = Object.keys(counts).find((key) => counts[key] % 2)
+//  return Number(odd);
 
-}
+// }
 
 
-console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]))
+// console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]))
+
+
+const findOdd = (arr) => arr.reduce((a, b) => a ^ b);
+
+console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]));
