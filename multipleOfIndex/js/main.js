3c94760ae4f711daa8898396cd86d8cd2610f12e
Tim Lines <timlines89@gmail.com>
2025-01-10 18:32:31 -0600

    multipleOfIndex

---
+++ multipleOfIndex/js/main.js
@@ -0,0 +1,26 @@
+function multipleOfIndex(array){
+
+
+    let result = [];
+
+    for ( let i = 0; i < array.length; i++ ){
+
+        if ( array[i] == 0 || array[i] % array.indexOf(array[i]) === 0 ) {
+            result.push(array[i]);
+        }
+    }
+
+    return result;
+    
+   
+}
+
+// if false that means it is a multiple of
+
+
+console.log(multipleOfIndex([22, -6, 32, 82, 9, 25])); // [-6, 32, 25]
+
+
+// function multipleOfIndex(array) {
+//     return array.filter((x, i) => x == 0 || x % i === 0)
+// }
