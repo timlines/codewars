41126645f4512ce4ab10de0d68cfe458cb49aac2
Tim Lines <timlines89@gmail.com>
2025-01-27 12:16:25 -0600

    dnastrand

---
+++ dnaStrand/js/main.js
@@ -0,0 +1,58 @@
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
+
+
+// function dnaStrand(dna){
+
+
+//     let result = '';
+
+
+//     for (let i = 0; i < dna.length; i++ ){
+//         if ( dna[i] === 'A' ) {
+//             result += 'T'
+//         }else if ( dna[i] === 'T'){
+//             result += 'A'
+//         }else if ( dna[i] === 'G' ){
+//             result += 'C'
+//         }else if ( dna[i] === 'C' ){
+//             result += 'G'
+//         }else{
+//             return 'Error, enter A, C, T, G'
+//         }
+//     }
+
+//     return result;
+    
+    
+
+
+// }
+
+
+function DNAStrand(dna){
+    return dna.replace(/./g, function(c){
+        return DNAStrand.pairs[c]
+    })
+}
+
+DNAStrand.pairs = {
+    A: 'T',
+    T: 'A',
+    C: 'G',
+    G: 'C',
+}
+
+
+console.log(DNAStrand('AAAA'));
