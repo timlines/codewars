7acf7e9b954f8903b9b6299dc11feee1569dafe2
Tim Lines <timlines89@gmail.com>
2025-01-28 12:59:23 -0600

    XO edits

--- XO/js/main.js
+++ XO/js/main.js
@@ -24,5 +24,3 @@ console.log(XO("xooxx") ) // / => false
 console.log(XO("ooxXm") ) // / => true
 console.log(XO("zpzpzpp" )) //  // => true // when no 'x' and 'o' is present should return true
 console.log(XO("zzoo")  ) //  => false
-
-
