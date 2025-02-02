cc3f6cec4ad1012f6dedaa47f955d67341aed247
Tim Lines <timlines89@gmail.com>
2025-02-02 12:44:40 -0600

    tidyNumber with [n +]

--- tidyNumber/js/main.js
+++ tidyNumber/js/main.js
@@ -1,9 +1,40 @@
+// Definition
+// A Tidy Number is a number whose digits are in non-decreasing order.
+
+// Task
+// Given a number, determine if it is tidy or not.
+
+// Notes
+// The number passed will always be positive.
+// Return the result as a boolean.
+// Examples
+// 12 ==> return true
+// Explanation: Digits {1, 2} are in non-decreasing order (1 <= 2).
+
+// 32 ==> return false
+// Explanation: Digits {3, 2} are not in non-decreasing order (3 > 2).
+
+// 1024 ==> return false
+// Explanation: Digits {1, 0, 2, 4} are not in non-decreasing order (1 > 0).
+
+// 13579 ==> return true
+// Explanation: Digits {1, 3, 5, 7, 9} are in non-decreasing order.
+
+// 2335 ==> return true
+// Explanation: Digits {2, 3, 3, 5} are in non-decreasing order (3 = 3).
+
+// function tidyNumber(n){
+//     let s = n.toString();
+//     for (let i = 0; i < s.length-1; i++){
+//         if (s[i] > s[i+1]) return false;
+//     }
+//     return true;
+// }
+
+
 function tidyNumber(n){
-    let s = n.toString();
-    for (let i = 0; i < s.length-1; i++){
-        if (s[i] > s[i+1]) return false;
-    }
-    return true;
+    return [...n+''].sort().join``==n
 }
 
+console.log(tidyNumber(12))
 
