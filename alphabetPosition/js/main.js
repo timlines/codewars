07d3572f79b65c1c1cbd07d5ab9e1c6b93307a36
Tim Lines <timlines89@gmail.com>
2025-02-03 11:42:22 -0600

    order

--- alphabetPosition/js/main.js
+++ alphabetPosition/js/main.js
@@ -1,6 +1,6 @@
 let alphabet = ['0','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
+// Not yet working
 function alphabetPositon(text){
 
     return text.toLowerCase().replace(/./g, function(c) {
