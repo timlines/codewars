8fd4de6073df0a97ce79db1cb689e5e1fd972fd2
Tim Lines <timlines89@gmail.com>
2025-02-08 13:38:52 -0600

    arrCheck with every

--- arrCheck/js/main.js
+++ arrCheck/js/main.js
@@ -1,17 +1,31 @@
-const arrCheck = value => {
+// Is every value in the array an array?
 
-    let result = [];
+// This should only test the second array dimension of the array. The values of the nested arrays don't have to be arrays.
 
-    for ( let element of value ){
-        result.push(Array.isArray(element));
+// Examples:
 
-    }
+// [[1],[2]] => true
+// ['1','2'] => false
+// [{1:1},{2:2}] => false
 
-    return result.every(element => element === true);
+// const arrCheck = value => {
+
+//     let result = [];
+
+//     for ( let element of value ){
+//         result.push(Array.isArray(element));
+
+//     }
+
+//     return result.every(element => element === true);
     
 
 
-}
+// }
+
+const arrCheck = value => value.every(Array.isArray);
+
+
 
 
 
