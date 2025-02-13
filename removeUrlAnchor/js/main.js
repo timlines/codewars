2ac84a4891829ba61b94819cfdb9610e661906c2
Tim Lines <timlines89@gmail.com>
2025-02-13 16:29:42 -0600

    removeUrlAnchor

---
+++ removeUrlAnchor/js/main.js
@@ -0,0 +1,13 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+
+function removeUrlAnchor(url){
+    return url.split('#')[0]
+}
+
+console.log(removeUrlAnchor('www.codewars.com#about'));
+console.log(removeUrlAnchor('www.codewars.com?page=1'));
