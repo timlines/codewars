ce05956a1cbf5b62df50d30f2dd5b2848bf06690
Tim Lines <timlines89@gmail.com>
2025-02-20 16:22:31 -0600

    isvow2 v2

--- isVow2/js/main.js
+++ isVow2/js/main.js
@@ -1,22 +1,40 @@
-const vowel = {
-    97: 'a',
-    101: 'e',
-    105: 'i', 
-    111: 'o',
-    117: 'u'
-}
+// const vowel = {
+//     97: 'a',
+//     101: 'e',
+//     105: 'i', 
+//     111: 'o',
+//     117: 'u'
+// }
 
 
-function isVow(a){
-    //return vowel[a[0]]
+// function isVow(a){
+//     //return vowel[a[0]]
 
 
-    let result = []
-    for ( let number of a){
-        result.push( vowel[number] || number)
+//     let result = []
+//     for ( let number of a){
+//         result.push( vowel[number] || number)
+//     }
+//     return result;
+// }
+
+
+
+
+
+//v2
+
+const isVow = a =>{
+    let map = {
+      97: 'a',
+      101: 'e',
+      105: 'i',
+      111: 'o',
+      117: 'u',
     }
-    return result;
-}
+    return a.map( num => map[num] ? map[num] : num);
+  }
+
 
 console.log(isVow([97, 1, 2, 3]))
 console.log(isVow([118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106]))
