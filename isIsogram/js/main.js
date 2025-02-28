b1e097dae52cec6f4065285942fee131dc534174
Tim Lines <timlines89@gmail.com>
2025-02-28 13:23:07 -0600

    isIsogram with Set()

--- isIsogram/js/main.js
+++ isIsogram/js/main.js
@@ -6,33 +6,38 @@
 // "aba" --> false
 // "moOse" --> false (ignore letter case)
 
+// function isIsogram(str){
+
+//     // only work with lowercase strings to avoid case sensative issues
+//     str = str.toLowerCase();
+
+//     // check if each letter occurs more than once
+
+//     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//     for ( let letter of alphabet ) {
+//         let charCount = 0;
+//         // count the number of times each letters apears
+//         // console.log(letter)
+//         charCount = str.split('').filter((char) => char === letter).join('').length;
+
+//         // console.log(charCount)
+
+//         // if a count is greater than 1 then return false
+//         if ( charCount > 1 ){
+//             return false
+//         }
+
+//     }
+    
+//     return true; 
+
+    
+// }
+
+
 function isIsogram(str){
-
-    // only work with lowercase strings to avoid case sensative issues
-    str = str.toLowerCase();
-
-    // check if each letter occurs more than once
-
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-    for ( let letter of alphabet ) {
-        let charCount = 0;
-        // count the number of times each letters apears
-        // console.log(letter)
-        charCount = str.split('').filter((char) => char === letter).join('').length;
-
-        // console.log(charCount)
-
-        // if a count is greater than 1 then return false
-        if ( charCount > 1 ){
-            return false
-        }
-
-    }
-    
-    return true; 
-
-    
+    return new Set(str.toUpperCase()).size  == str.length;
 }
 
 
