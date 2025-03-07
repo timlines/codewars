72f7d83a7d56359107226b43547fc43ab912f794
Tim Lines <timlines89@gmail.com>
2025-03-07 14:27:25 -0600

    testEven

---
+++ testEven/js/main.js
@@ -0,0 +1,10 @@
+function testEven(n){
+    return n % 2 ? false : true;
+}
+
+
+console.log(testEven(0)) // returns true
+console.log(testEven(0.5)) // returns false
+console.log(testEven(1)) // returns false
+console.log(testEven(2)) // returns true
+console.log(testEven(-4)) // returns true
